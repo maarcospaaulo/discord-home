434d8ea531cc142da56bc5bd227f13b6ebefa547
maarcospaaulo <maarcospaaulo@live.com>
2021-02-27 14:01:37 -0300

    create component routes

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { BrownserRouter, Route, Switch } from 'react-router-dom'
+
+const Routes = () => (
+    <BrownserRouter>
+        <Switch>
+            <Route exact path="/" component={} />
+        </Switch>
+    </BrownserRouter>
+);
+
+export default Routes;
